8ed332e1840749f7f2b896dcdd4acadd982aa338
John <johnsief12@gmail.com>
2023-12-15 18:00:25 -0500

    added game function to add/remove to start game button

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello World');
+const start = document.querySelector(".start");
+const landingPage = document.querySelector(".landing-page");
+const questionPage = document.querySelector(".question-page");
+//when button is click game starts
+//landing page to be hidden and show game page.
+const startGame = () => {
+landingPage.classList.add('hidden');
+questionPage.classList.remove("hidden");
+}
+
+start.addEventListener("click", startGame)
